9307e36e2f44cab21b2cfb25fb338098141eae11
Adam Czech <adamc3123@gmail.com>
2020-11-16 17:04:06 -0600

    Add zsh config

---
+++ zsh/.aliases.zsh
@@ -0,0 +1,10 @@
+# Keep command output in terminal after closing
+LESS='-R --no-init --quit-if-one-screen'
+
+# Always grep with color
+alias grep='grep --color=auto'
+
+#
+# git
+#
+alias undo_commit_save_changes='git reset HEAD^'
